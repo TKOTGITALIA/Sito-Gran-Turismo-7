73dffcfc5461b43065c1d81452e14903f2a8306e
TKOTGITALIA <loccistefano11@gmail.com>
2026-02-14 21:41:55 +0100

    Updated script.js
    Solved a problem with the loading icon

--- script.js
+++ script.js
@@ -230,11 +230,14 @@ async function caricaDati() {
         gestisciVisibilitaExtra();
 
         if (loader) {
-            loader.style.opacity = '0';
             setTimeout(() => {
-                loader.style.display = 'none';
-            }, 500);
+                loader.style.opacity = '0';
+                setTimeout(() => {
+                    loader.style.display = 'none';
+                }, 500);
+            }, 1000); 
         }
+
     } catch(e) { 
         console.error(e);
         if (loader) {
